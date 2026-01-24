b722154cb43fa1c03bf4843a7478e9aa5e5afed5
krishaswal-cell <krishaswal2006@gmail.com>
2026-01-24 12:45:40 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+ function changeColourfirst(){
+    const a = document.getElementById('box-1');
+    a.style.backgroundColor="red";
+ }
+
+ function changeColoursecond(){
+    const b = document.getElementById('box-2');
+    b.style.backgroundColor="blue";
+ }
+
+ function changeColourthird(){
+    const c = document.getElementById('box-3');
+    c.style.backgroundColor="green";
+ }
+
+ function changeColourfourth(){
+    const d = document.getElementById('box-4');
+    d.style.backgroundColor="yellow";
+ }
+
+
+function addname(){
+      const name = document.getElementById('btn');
+      name.addEventListener('click', function (){
+      const userName = document.getElementById('username').value;
+      const heading = document.getElementById('name');
+      heading.innerText = userName;
+      });
+}
+
